1dd45b52424e129b33bc4d3cf5647d8943dafbe6
TsioryR98 <hei.tsiory.2@gmail.com>
2025-01-26 18:12:26 +0100

    fix: fix context to use array of object to access all datas from API

--- cartProductUI/src/context/ProductDataContext.jsx
+++ cartProductUI/src/context/ProductDataContext.jsx
@@ -1,60 +1,75 @@
 import { useContext, useEffect, useState, createContext } from "react";
 import {
-  fecthTechData,
-  fecthMenData,
-  fecthWomenData,
-  fecthBodyData,
-  fecthDecoData,
+  fetchTechData,
+  fetchMenData,
+  fetchWomenData,
+  fetchBodyData,
+  fetchDecoData,
 } from "../api/productAPI";
 
 const ProductDataContext = createContext();
 
 const ProductDataProvider = ({ children }) => {
-  /* const [allDataProduct, setAllDataProduct] = useState([
-    { techData: [] },
-    { menData: [] },
-    { womenData: [] },
-    { bodyData: [] },
-    { decoData: [] },
-  ]);
-  */
-  const [techData, setTechData] = useState([]);
-  const [menData, setMenData] = useState([]);
-  const [womenData, setWomenData] = useState([]);
-  const [bodyData, setBodyData] = useState([]);
-  const [decoData, setDecoData] = useState([]);
+  /*
+  put combinedProduct as an array of object
+  responseData is an array
 
-  const getDataProuct = async () => {
+  Another solution to put as an object in the global state
+   */
+  const [categoryProductData, setCategoryProductData] = useState([
+    { category: "Technology", responseData: [] },
+    { category: "Men Dress", responseData: [] },
+    { category: "Women Dress", responseData: [] },
+    { category: "Body Care", responseData: [] },
+    { category: "Decoration", responseData: [] },
+  ]);
+  const getDataProduct = async () => {
     try {
       // get all dataproduct from fetch functions
 
       // promise async
       const [tecProduct, menProduct, womenProduct, bodyProduct, decoData] =
         await Promise.all([
-          fecthTechData(),
-          fecthMenData(),
-          fecthWomenData(),
-          fecthBodyData(),
-          fecthDecoData(),
+          fetchTechData(),
+          fetchMenData(),
+          fetchWomenData(),
+          fetchBodyData(),
+          fetchDecoData(),
         ]);
-      setTechData(tecProduct);
-      setMenData(menProduct);
-      setWomenData(womenProduct);
-      setBodyData(bodyProduct);
-      setDecoData(decoData);
+      setCategoryProductData([
+        {
+          category: "Technology",
+          responseData: tecProduct,
+        },
+        {
+          category: "Men Dress",
+          responseData: menProduct,
+        },
+        {
+          category: "Women Dress",
+          responseData: womenProduct,
+        },
+        {
+          category: "Body Care",
+          responseData: bodyProduct,
+        },
+        {
+          category: "Decoration",
+          responseData: decoData,
+        },
+      ]);
     } catch (error) {
       console.error(error);
     }
   };
   //outside the async function
   useEffect(() => {
-    getDataProuct();
+    getDataProduct();
   }, []);
 
   return (
-    <ProductDataContext.Provider
-      value={{ techData, menData, womenData, bodyData, decoData }}
-    >
+    //golbal state for categoryProductData instead of each datas
+    <ProductDataContext.Provider value={{ categoryProductData }}>
       {children}
     </ProductDataContext.Provider>
   );
